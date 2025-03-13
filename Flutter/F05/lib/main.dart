e02ab6306cf3acc73eb58489a7557d4106b045d8
HWKKK <110362551+HWKKK@users.noreply.github.com>
2025-03-13 16:47:10 +0900

    Add files via upload

--- Flutter/F05/lib/main.dart
+++ Flutter/F05/lib/main.dart
@@ -1,3 +1,10 @@
+/*
+[회고]
+- 박지윤 : 정말 잘 안 된다. 살려주세요.
+- 김해원 : 아 역대급 힘들었습니다. 파이썬 포트 넘버를 잘못 설정해놓고 왜 안되지 한참 고민했습니다. 하나 배웠습니다
+*/
+
+
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
